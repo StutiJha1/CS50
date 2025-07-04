49eba3d0b5ac102b69b09a39444d17e539f2d782
Stuti Jha <stutijha@sj-macbook-pro.home>
2025-07-04 09:53:59 -0400

    updated

--- Search/Projects_0/tictactoe/tictactoe.py
+++ Search/Projects_0/tictactoe/tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+from copy import deepcopy
 
 X = "X"
 O = "O"
@@ -10,58 +11,83 @@ EMPTY = None
 
 
 def initial_state():
-    """
-    Returns starting state of the board.
-    """
     return [[EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY]]
 
 
 def player(board):
-    """
-    Returns player who has the next turn on a board.
-    """
-    raise NotImplementedError
+    flat = sum(board, [])
+    x_count = flat.count(X)
+    o_count = flat.count(O)
+    return X if x_count == o_count else O
 
 
 def actions(board):
-    """
-    Returns set of all possible actions (i, j) available on the board.
-    """
-    raise NotImplementedError
+    return {(i, j) for i in range(3) for j in range(3) if board[i][j] == EMPTY}
 
 
 def result(board, action):
-    """
-    Returns the board that results from making move (i, j) on the board.
-    """
-    raise NotImplementedError
+    i, j = action
+    if board[i][j] != EMPTY:
+        raise ValueError("Invalid move")
+    new_board = deepcopy(board)
+    new_board[i][j] = player(board)
+    return new_board
 
 
 def winner(board):
-    """
-    Returns the winner of the game, if there is one.
-    """
-    raise NotImplementedError
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] != EMPTY:
+            return board[i][0]
+        if board[0][i] == board[1][i] == board[2][i] != EMPTY:
+            return board[0][i]
+    if board[0][0] == board[1][1] == board[2][2] != EMPTY:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != EMPTY:
+        return board[0][2]
+    return None
 
 
 def terminal(board):
-    """
-    Returns True if game is over, False otherwise.
-    """
-    raise NotImplementedError
+    return winner(board) is not None or all(cell is not EMPTY for row in board for cell in row)
 
 
 def utility(board):
-    """
-    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
-    """
-    raise NotImplementedError
+    win = winner(board)
+    if win == X:
+        return 1
+    elif win == O:
+        return -1
+    return 0
 
 
 def minimax(board):
-    """
-    Returns the optimal action for the current player on the board.
-    """
-    raise NotImplementedError
+    if terminal(board):
+        return None
+
+    current = player(board)
+
+    def max_value(b):
+        if terminal(b):
+            return utility(b), None
+        v = -math.inf
+        best_action = None
+        for action in actions(b):
+            min_result, _ = min_value(result(b, action))
+            if min_result > v:
+                v, best_action = min_result, action
+        return v, best_action
+
+    def min_value(b):
+        if terminal(b):
+            return utility(b), None
+        v = math.inf
+        best_action = None
+        for action in actions(b):
+            max_result, _ = max_value(result(b, action))
+            if max_result < v:
+                v, best_action = max_result, action
+        return v, best_action
+
+    return max_value(board)[1] if current == X else min_value(board)[1]
